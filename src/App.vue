cd74eb9aebfb1a6f887408cb851a6fc856fbebeb
Rechzz <reshmareghu.ptf@gmail.com>
2024-07-08 11:37:53 +0530

    latest

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
-  <!-- <v-app>
-    <v-main> -->
+  <v-app>
+    <v-main>
        <router-view></router-view>
-    <!-- </v-main>
-  </v-app> -->
+    </v-main>
+  </v-app>
 </template>
 
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+</style>
--- src/components/TopicCard.vue
+++ src/components/TopicCard.vue
@@ -1,14 +1,26 @@
 <template>
-  <div v-for="topic in Topics" :key="topic.uId"
-    class="image-container mx-auto my-5 d-flex flex-column justify-content-end ps-5 pb-5" :style="{'background-image': `linear-gradient(to right, rgba(0, 0, 0, 0.982), rgba(37, 37, 37, 0.253)), ${getBackgroundImage(topic)}`
-    }" @click="goToTopic(topic[0].commonId)">
-    <div v-if="topic" class="image-details ms-5 pb-5">
+  <v-card v-for="topic in Topics" :key="topic.uId"
+    class="image-container d-flex flex-column" :style="{'background-image': `linear-gradient(to right, rgba(0, 0, 0, 0.982), rgba(37, 37, 37, 0.253)), ${getBackgroundImage(topic)}`
+    }" elevation="24">
+    <div v-if="topic" class="image-details">
       <h1 class="image-title">{{ topic[0].title }}</h1>
       <div class="description-container text-white">
         <p v-html="topic[0].description"></p>
       </div>
+
     </div>
-  </div>
+
+    <div @click="goToTopic(topic[0].commonId)" class="d-flex scrollright-container">
+      <p class="text-white read-more" style="font-size: 18px;" >Read More...</p>
+        <div class="scrollright-btn">
+          <v-icon class="first-arrow" size="40">mdi-chevron-right</v-icon>
+          <v-icon class="second-arrow" size="40">mdi-chevron-right</v-icon>
+          <v-icon class="third-arrow" size="40">mdi-chevron-right</v-icon>
+          <v-icon class="fourth-arrow" size="40">mdi-chevron-right</v-icon>
+        </div>
+    </div>
+
+  </v-card>
 </template>
 
 <script>
@@ -64,52 +76,53 @@ export default {
 </script>
 
 <style scoped>
+
 .image-container {
   background-size: cover;
   width: 100%;
-  aspect-ratio: 1936 / 848;
-  border-radius: 25px;
+  aspect-ratio: 1948 / 800;
+  border-radius: 20px;
+  margin-bottom: 5%;
   position: relative;
-  cursor: pointer;
 }
 .image-details {
   font-weight: 400;
-  width: 80%;
-  height: 300px;
-  aspect-ratio: 942/277;
+  width: 85%;
+  /* height: 200px; */
+  position:absolute;
+  bottom:20%;
+  aspect-ratio: auto;
+  left:7%;
 }
 
 .image-title {
-  font-size: 45px;
-  font-weight: bold;
+  font-size: 40px;
+  font-weight: 600;
+  line-height: 40px;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
   color: white;
 }
 
 .description-container {
   overflow: hidden;
-  height: 180px;
-  width:70%;
+  /* height: 180px; */
+  width:80%;
   
 }
 .description-container p{
   overflow: hidden;
   text-overflow: ellipsis;
-word-wrap: break-word;
-font-size: 100%;
-line-height: 32px;
+  word-wrap: break-word;
+  /* font-size: 20px; */
+  line-height: 32px;
 }
 .image-description {
-  font-size: 13px;
+  font-size: 20px;
   line-height: 1.4;
   color: white;
   width: 500px;
 }
-.see-more {
-    color: white;
-    cursor: pointer;
-    text-decoration: none;
-    line-height: 32px;
-}
+
 :deep(pre) {
   text-wrap: wrap;
   overflow-y: hidden;
@@ -117,11 +130,78 @@ line-height: 32px;
   min-height: auto;
   max-height: 180px;
   padding-right: 5px;
-  font-family: Arial, Helvetica, sans-serif;
+  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
   text-align: justify;
-  font-size: 16px;
+  font-size: 20px;
   display: -webkit-box;
-    -webkit-line-clamp: 4;
-    -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
 }
+.scrollright-container {
+  background: transparent;
+  position: absolute;
+  left:7.5%;
+  bottom: 8%;
+  cursor: pointer;
+}
+.scrollright-btn {
+  width: 100px;
+  height: 50px;
+  position: absolute;
+  left: 125%;
+  top: 32%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+.v-icon {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  color: white;
+}
+.first-arrow {
+  animation: scrollanim 1s ease-in-out infinite;
+  animation-delay: 0.8s;
+}
+.second-arrow {
+  animation: scrollanim 1s ease-in-out infinite;
+  animation-delay: 0.6s;
+}
+.third-arrow {
+  animation: scrollanim 1s ease-in-out infinite;
+  animation-delay: 0.4s;
+}
+.fourth-arrow {
+  animation: scrollanim 1s ease-in-out infinite;
+  animation-delay: 0.2s;
+}
+@keyframes scrollanim {
+  0% {
+    transform: translateY(-50%) translateX(-40px);
+    opacity: 0;
+  }
+  60% {
+    transform: translateY(-50%) translateX(0);
+    opacity: 0.8;
+  }
+}
+@keyframes scaleUpDown {
+
+  0%,
+  100% {
+    transform: scale(1);
+  }
+
+  50% {
+    transform: scale(1.1);
+  }
+}
+ @keyframes shine {
+   to {
+     background-position: 200% center;
+   }
+ }
+/* .read-more {
+  animation: scaleUpDown 1.5s infinite;
+} */
 </style>
