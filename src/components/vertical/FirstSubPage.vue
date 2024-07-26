5ba15817d62ce5357778c09f470cdf518262c41c
Rechzz <reshmareghu.ptf@gmail.com>
2024-07-26 11:46:04 +0530

    "Updated background image styles in FirstSubPage, MainPage, and SecondSubPage components"

--- src/components/vertical/FirstSubPage.vue
+++ src/components/vertical/FirstSubPage.vue
@@ -8,7 +8,7 @@
 
       <div class=" card mb-3">
         <div class="main-card p-4" :style="{
-  'background-image': `radial-gradient(circle at 16% 83%, rgba(148, 148, 148,0.06) 0%, rgba(148, 148, 148,0.06) 50%,rgba(63, 63, 63,0.06) 50%, rgba(63, 63, 63,0.06) 100%),radial-gradient(circle at 68% 87%, rgba(66, 66, 66,0.06) 0%, rgba(66, 66, 66,0.06) 50%,rgba(105, 105, 105,0.06) 50%, rgba(105, 105, 105,0.06) 100%),radial-gradient(circle at 38% 50%, rgba(123, 123, 123,0.06) 0%, rgba(123, 123, 123,0.06) 50%,rgba(172, 172, 172,0.06) 50%, rgba(172, 172, 172,0.06) 100%),linear-gradient(90deg, rgba(12,3,3, 0.93),rgba(12,3,3, 0.93)),${getBackgroundImage(topic)}`
+  'background-image': `radial-gradient(circle at center center, rgba(28,27,27, 0.78),rgba(0,0,0, 0.69)),${getBackgroundImage(topic)}`
         }">
 
           <div style="" class="mt-4 full-desc">
