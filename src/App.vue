0d458eac4c2d1a9fcc5956c280f57b540f874bc4
Rechzz <reshmareghu.ptf@gmail.com>
2024-11-07 13:03:07 +0530

    Revert "condition changed app.vue"
    This reverts commit 74c0ec1312b732601f6321ed280f57e7019173ea.

--- src/App.vue
+++ src/App.vue
@@ -31,17 +31,17 @@ export default {
     handleInactivity() {
       console.log('inactivity', this.$store.getters.getSelectedCommonIds.length)
       if (!this.isVideoPlaying) {
-        if (this.$route.path != '/digitalBoard/' && this.$store.getters.getSelectedTopics.length > 1 > 1) {
+        if (this.$route.path != '/digitalBoard/' && this.$store.getters.getSelectedCommonIds.length > 1) {
         this.$store.commit('setResetTime', false);
         this.$router.push('/digitalBoard/selectedTopics');
       }
 
-        if ((this.$route.path != '/digitalBoard/detailsPage' && this.$route.path != '/digitalBoard/') && this.$store.getters.getSelectedTopics.length == 1) {
+        if ((this.$route.path != '/digitalBoard/detailsPage' && this.$route.path != '/digitalBoard/') && this.$store.getters.getSelectedCommonIds.length == 1) {
         this.$store.commit('setResetTime', false);
         this.$router.push('/digitalBoard/detailsPage');
       }
       
-        if ((this.$route.path == '/digitalBoard/detailsPage' && this.$route.path != '/digitalBoard/') && this.$store.getters.getSelectedTopics.length == 1) {
+        if ((this.$route.path == '/digitalBoard/detailsPage' && this.$route.path != '/digitalBoard/') && this.$store.getters.getSelectedCommonIds.length == 1) {
         this.$store.commit('setResetTime', true);
       }
     } else {
